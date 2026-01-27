e354b8eeebeae63d799c88273810fc5600517fbc
KistDeovCharter <kistuttosi.david@charter.hu>
2026-01-27 15:32:25 +0100

    minden fix végre

--- src/lib/prismaDB.ts
+++ src/lib/prismaDB.ts
@@ -1,13 +1,17 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const adapter = new PrismaMariaDb();
+
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ['query'], 
+    log: ['query'],
+    adapter,
   });
 
 if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
