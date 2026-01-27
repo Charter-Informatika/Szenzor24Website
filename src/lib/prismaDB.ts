4f3402ea189f7fb99b4e857810f791f3ee0ee537
KistDeovCharter <kistuttosi.david@charter.hu>
2026-01-27 11:52:03 +0100

    prisma db

--- src/lib/prismaDB.ts
+++ src/lib/prismaDB.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
-
-export default prisma;
+export const prisma = new PrismaClient();
